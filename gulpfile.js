968637109e8af398dd550c48a1ad661022edd977
Ejurov Konstantin <uchiha_ej@mail.ru>
2021-11-18 08:19:55 +0300

    added svg sprites

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,22 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
+
+
+
+
+function svgsprite() {
+  return src('app/images/icons/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg'
+        }
+      }
+    }))
+    .pipe(dest('app/images'));
+}
 
 function browsersync() {
   browserSync.init({
@@ -42,6 +58,7 @@ function styles() {
 function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -96,6 +113,7 @@ function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/**/*.svg'], svgsprite);
 }
 
 exports.styles = styles;
@@ -105,5 +123,6 @@ exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
+exports.svgSprite = svgSprite;
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, browsersync, svgsprite, watching);
